53d0d52811f0bcf5a08be8ba1815904196e64e69
Rinat <lich666black@gmail.com>
2020-10-18 19:48:17 +0500

    added controllers template

---
+++ src/lib/controller/wallet.ts
@@ -0,0 +1,9 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+export class Wallet {}
