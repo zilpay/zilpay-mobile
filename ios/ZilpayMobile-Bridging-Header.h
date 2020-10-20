8d5ff0d02214be875327b573b4aa6e4d3634d3e7
Alexander B <alex.batalov@workcall.com>
2020-10-20 20:43:10 +0300

    Test big num swift interface

---
+++ ios/ZilpayMobile-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  ZilpayMobile-Bridging-Header.h
+//  zilpayMobile
+//
+//  Created by Alexander Batalov on 10/20/20.
+//
+
+#ifndef ZilpayMobile_Bridging_Header_h
+#define ZilpayMobile_Bridging_Header_h
+
+#import <TrezorCrypto/TrezorCrypto.h>
+#import <React/RCTBridgeModule.h>
+
+#endif /* ZilpayMobile_Bridging_Header_h */
+
--- ios/zilpayMobile/AppDelegate.m
+++ ios/zilpayMobile/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <TrezorCrypto/TrezorCrypto.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
