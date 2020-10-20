961c471c7a82f880c7e702839217ea50eee506bc
Rinat <lich666black@gmail.com>
2020-10-20 13:13:40 +0500

    addeed shim.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /**
  * @format
  */
+import './shim';
+
+require('react-native-browser-polyfill');
+
 import { AppRegistry } from 'react-native';
 import root from './src/root';
 import { name as appName } from './app.json';
