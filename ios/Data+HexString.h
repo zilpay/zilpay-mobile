084c75e3eb792ad37dab554cd8ef74080a864060
Rinat <lich666black@gmail.com>
2020-10-24 17:31:20 +0500

    added DataHex native module

---
+++ ios/Data+HexString.h
@@ -0,0 +1,11 @@
+//
+//  Data+HexString.h
+//  zilpayMobile
+//
+//  Created by Rinat on 24.10.2020.
+//
+#import <Foundation/Foundation.h>
+
+@interface NSData(HexString)
+-(NSString *)hexString;
+@end
