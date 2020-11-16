11701ba655cf0046384652719bc41da9c827e96d
Rinat <lich666black@gmail.com>
2020-11-16 21:52:56 +0500

    fixed routers

--- src/router.tsx
+++ src/router.tsx
@@ -7,124 +7,131 @@
  * Copyright (c) 2020 ZilPay
  */
 import * as React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createAppContainer, createSwitchNavigator, NavigationScreenConfig } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import CreateWalletPage from 'app/pages/create-wallet';
+import LetStartPage from 'app/pages/let-start';
 import GetStartedPage from 'app/pages/get-started';
 import LockPage from 'app/pages/lock';
 import RestorePage from 'app/pages/restore';
 import PrivacyPage from 'app/pages/privacy';
 import MnemonicGenPage from 'app/pages/mnemonic-gen';
-import MnemonicVerifypage from 'app/pages/mnemonic-verify';
+import MnemonicVerifyPage from 'app/pages/mnemonic-verify';
 import SetupPasswordPage from 'app/pages/setup-password';
 import InitSuccessfullyPage from 'app/pages/init-successfully';
 import HomePage from 'app/pages/home';
+import AuthLoadingPage from 'app/pages/auth-loading';
 
-import { WalletContext } from './keystore';
 import { theme } from './styles';
-import i18n from 'app/lib/i18n';
 
-export type RootStackParamList = {
-  GetStarted: undefined;
-  Privacy: undefined;
-  Create: undefined;
-  Lock: undefined;
-  Restore: undefined;
-  Mnemonic: undefined;
-  MnemonicVerif: {
-    phrase: string
-  };
-  SetupPassword: {
-    phrase: string
-  };
-  InitSuccessfully: undefined;
-  Home: undefined;
-};
-
-const Stack = createStackNavigator<RootStackParamList>();
-
-export default function Navigator() {
-  const keystore = React.useContext(WalletContext);
-
-  const initialRouteName = React.useMemo(() => {
-    const { isEnable } = keystore.guard;
-
-    if (!isEnable) {
-      return 'GetStarted';
+const AppStack = createStackNavigator({
+  Home: {
+    screen: HomePage,
+    navigationOptions: {
+      header: () => null,
+      title: ''
     }
-
-    return 'Lock';
-  }, [keystore]);
-
-  const HanldeRoute = React.useCallback(({ route, navigation }) => {
-    return {
-      headerShown: false,
-      gestureEnabled: true,
-      cardOverlayEnabled: true,
+  }
+});
+const AuthStack = createStackNavigator({
+  GetStarted: {
+    screen: GetStartedPage,
+    navigationOptions: {
+      header: () => null,
+      title: ''
+    }
+  },
+  Lock: {
+    screen: LockPage,
+    navigationOptions: {
+      header: () => null,
+      title: ''
+    }
+  },
+  SetupPassword: {
+    screen: SetupPasswordPage,
+    navigationOptions: {
+      header: () => null
+    }
+  },
+  InitSuccessfully: {
+    screen: InitSuccessfullyPage,
+    navigationOptions: {
+      header: () => null
+    }
+  },
+  Privacy: {
+    screen: PrivacyPage,
+    navigationOptions: {
       headerTintColor: theme.colors.white,
       headerStyle: {
         backgroundColor: theme.colors.black
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold'
       }
-    };
-  }, []);
+    }
+  },
+  LetStart: {
+    screen: LetStartPage,
+    navigationOptions: {
+      title: '',
+      headerTintColor: theme.colors.white,
+      headerStyle: {
+        backgroundColor: theme.colors.black
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  },
+  Restore: {
+    screen: RestorePage,
+    navigationOptions: {
+      headerTintColor: theme.colors.white,
+      headerStyle: {
+        backgroundColor: theme.colors.black
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  },
+  Mnemonic: {
+    screen: MnemonicGenPage,
+    navigationOptions: {
+      headerTintColor: theme.colors.white,
+      headerStyle: {
+        backgroundColor: theme.colors.black
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  },
+  MnemonicVerif: {
+    screen: MnemonicVerifyPage,
+    navigationOptions: {
+      headerTintColor: theme.colors.white,
+      headerStyle: {
+        backgroundColor: theme.colors.black
+      },
+      headerTitleStyle: {
+        fontWeight: 'bold'
+      }
+    }
+  }
+});
 
-  return (
-    <Stack.Navigator
-      headerMode={'screen'}
-      initialRouteName={initialRouteName}
-      screenOptions={HanldeRoute}
-    >
-      {/* <Stack.Screen
-        name={'GetStarted'}
-        component={GetStartedPage}
-        options={{ title: '' }}
-      />
-      <Stack.Screen
-        name={'Privacy'}
-        component={PrivacyPage}
-        options={{ title: i18n.t('privacy_title') }}
-      />
-      <Stack.Screen
-        name={'Create'}
-        component={CreateWalletPage}
-        options={{ title: '' }}
-      />
-      <Stack.Screen
-        name={'Restore'}
-        component={RestorePage}
-        options={{ title: '' }}
-      />
-      <Stack.Screen
-        name={'Mnemonic'}
-        component={MnemonicGenPage}
-        options={{ title: '' }}
-      />
-      <Stack.Screen
-        name={'MnemonicVerif'}
-        component={MnemonicVerifypage}
-        options={{ title: '' }}
-      />
-      <Stack.Screen
-        name={'SetupPassword'}
-        component={SetupPasswordPage}
-        options={{ title: '' }}
-      /> */}
-      <Stack.Screen
-        name={'Lock'}
-        component={LockPage}
-      />
+const GuardStack = createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingPage,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+);
 
-      {/* guarded */}
-      {/* <Stack.Screen
-        name={'InitSuccessfully'}
-        component={InitSuccessfullyPage}
-        options={{ title: '' }}
-      /> */}
-      {/* <Stack.Screen
-        name={'Home'}
-        component={HomePage}
-        options={{ title: '' }}
-      /> */}
-    </Stack.Navigator>
-  );
-}
+export default createAppContainer(GuardStack);
