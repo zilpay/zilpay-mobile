5e1293ad56ef326064d4897e8d313e33e4635d11
Rinat <lich666black@gmail.com>
2020-11-23 16:27:34 +0500

    added custom button

---
+++ src/components/custom-button.tsx
@@ -0,0 +1,50 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+import React from 'react';
+import {
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ViewStyle
+} from 'react-native';
+import { theme } from 'app/styles';
+
+type Prop = {
+  style?: ViewStyle;
+  title: string;
+  onPress?: () => void;
+};
+
+export const CustomButton: React.FC<Prop> = ({ title, style, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.container, style]}
+      onPress={onPress}
+    >
+      <Text style={styles.text}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    paddingVertical: 11,
+    borderRadius: 8,
+    backgroundColor: theme.colors.primary,
+    alignItems: 'center'
+  },
+  text: {
+    color: theme.colors.black,
+    fontSize: 17,
+    lineHeight: 22
+  }
+});
