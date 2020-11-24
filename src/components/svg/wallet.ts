fae5ea79ff6a5d7e0d448dba7502731163bee45b
Rinat <lich666black@gmail.com>
2020-11-24 16:24:35 +0500

    added wallet icon

---
+++ src/components/svg/wallet.ts
@@ -0,0 +1,13 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+export const WalletIconSVG = `
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M2 6C2 4.34315 3.34315 3 5 3H18V5H5C4.44772 5 4 5.44772 4 6V18C4 18.5523 4.44772 19 5 19H18V16.9646C16.3039 16.7219 15 15.2632 15 13.5C15 11.7368 16.3039 10.2781 18 10.0354V8H7V6H18H19H20V10H22V12V15V17H20V20V21H19H5C3.34315 21 2 19.6569 2 18V6ZM20 12H18.5C17.6716 12 17 12.6716 17 13.5C17 14.3284 17.6716 15 18.5 15H20V12Z" fill="#666666"/>
+</svg>
+`;
