edc0b706f4ed7812ae3cf45143390b81fb0275ff
Rinat <lich666black@gmail.com>
2020-11-25 13:22:33 +0500

    added Token View

---
+++ src/components/transfer/styles.ts
@@ -0,0 +1,88 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+import { StyleSheet, Dimensions } from 'react-native';
+import { theme } from 'app/styles';
+
+const { width } = Dimensions.get('window');
+export default StyleSheet.create({
+  item: {
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  infoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: width - 100
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: theme.colors.black,
+    borderBottomWidth: 1
+  },
+  percentWrapper: {
+    justifyContent: 'space-around',
+    flexDirection: 'row',
+    paddingVertical: 5,
+    marginLeft: 8
+  },
+  percent: {
+    color: theme.colors.primary,
+    fontSize: 16,
+    lineHeight: 21
+  },
+  itemInfo: {
+    alignItems: 'flex-start',
+    width: width - 90,
+    borderBottomWidth: 1,
+    borderColor: theme.colors.black
+  },
+  receivinglabel: {
+    fontSize: 16,
+    lineHeight: 21,
+    width: width - 100,
+    color: '#8A8A8F'
+  },
+  receiving: {
+    marginTop: 30,
+    padding: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  addressAmount: {
+    fontSize: 13,
+    lineHeight: 17,
+    color: '#8A8A8F'
+  },
+  label: {
+    fontSize: 16,
+    lineHeight: 21,
+    color: '#8A8A8F',
+    marginBottom: 7
+  },
+  arrowIcon: {
+    transform: [{ rotate: '-90deg'}],
+    alignSelf: 'center'
+  },
+  textInput: {
+    fontSize: 17,
+    lineHeight: 22,
+    padding: 10,
+    color: theme.colors.white,
+    width: width - 100
+  },
+  nameAmountText: {
+    fontSize: 17,
+    lineHeight: 22,
+    color: theme.colors.white
+  }
+});
---
+++ src/components/transfer/token.tsx
@@ -0,0 +1,122 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Dimensions
+} from 'react-native';
+import { SvgXml, SvgCssUri } from 'react-native-svg';
+import { useStore } from 'effector-react';
+
+import {
+  ArrowIconSVG,
+  WalletIconSVG
+} from 'app/components/svg';
+import { TokensModal } from 'app/components/modals';
+
+import i18n from 'app/lib/i18n';
+import { keystore } from 'app/keystore';
+import { fromZil, toConversion } from 'app/filters';
+import { Token } from 'types';
+import { TOKEN_ICONS } from 'app/config';
+
+import styles from './styles';
+
+type Prop = {
+  tokens: Token[];
+  selected: number;
+  netwrok: string;
+  onSelect: (index: number) => void;
+};
+
+const { width } = Dimensions.get('window');
+export const TransferToken: React.FC<Prop> = ({
+  tokens,
+  selected,
+  netwrok,
+  onSelect
+}) => {
+  const settingsState = useStore(keystore.settings.store);
+  const currencyState = useStore(keystore.currency.store);
+
+  const [tokenModal, setTokenModal] = React.useState(false);
+
+  const token = React.useMemo(
+    () => tokens[selected],
+    [tokens, selected]
+  );
+  const converted = React.useMemo(() => {
+    const { balance, decimals } = token;
+    const { rate } = settingsState;
+    const convert = toConversion(balance[netwrok], rate, decimals);
+
+    return `${convert} ${currencyState}`;
+  }, [token, selected, netwrok, currencyState]);
+
+  return (
+    <React.Fragment>
+      <TouchableOpacity
+        style={styles.item}
+        onPress={() => setTokenModal(true)}
+      >
+        <SvgXml xml={WalletIconSVG} />
+        <View style={styles.itemInfo}>
+          <Text style={styles.label}>
+            {i18n.t('token')}
+          </Text>
+          <View style={{ flexDirection: 'row' }}>
+            <SvgCssUri
+              height="30"
+              width="30"
+              uri={`${TOKEN_ICONS}/${token.symbol}.svg`}
+            />
+            <View style={{ marginLeft: 5 }}>
+              <View style={[styles.infoWrapper, { width: width - 120 }]}>
+                <Text style={styles.nameAmountText}>
+                  {token.symbol}
+                </Text>
+                <Text style={styles.nameAmountText}>
+                  {fromZil(token.balance[netwrok], token.decimals)}
+                </Text>
+              </View>
+              <View style={[styles.infoWrapper, {
+                marginBottom: 15,
+                width: width - 120
+              }]}>
+                <Text style={styles.addressAmount}>
+                  {token.name}
+                </Text>
+                <Text style={styles.addressAmount}>
+                  {converted}
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <SvgXml
+          xml={ArrowIconSVG}
+          fill="#666666"
+          style={styles.arrowIcon}
+        />
+      </TouchableOpacity>
+      <TokensModal
+        title={i18n.t('transfer_modal_title0')}
+        visible={tokenModal}
+        network={netwrok}
+        tokens={tokens}
+        selected={selected}
+        onTriggered={() => setTokenModal(false)}
+        onSelect={onSelect}
+      />
+    </React.Fragment>
+  );
+};
