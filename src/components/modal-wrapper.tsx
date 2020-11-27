0cbb1b6ca7d538ced277fef65f33e46c5f0ab58c
Rinat <lich666black@gmail.com>
2020-11-27 14:08:20 +0500

    added modal-wrapper component

---
+++ src/components/modal-wrapper.tsx
@@ -0,0 +1,37 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+import React from 'react';
+import {
+  View,
+  StyleSheet,
+  ViewStyle
+} from 'react-native';
+
+type Prop = {
+  style?: ViewStyle;
+};
+
+export const ModalWrapper: React.FC<Prop> = ({ children, style }) => {
+  return (
+    <View style={[styles.container, style]}>
+      {children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+    borderTopEndRadius: 16,
+    borderTopStartRadius: 16,
+    backgroundColor: '#18191D',
+    justifyContent: 'space-between',
+    paddingVertical: 15
+  }
+});
