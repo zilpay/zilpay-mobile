8eba0ba9a235f45d719b966b2f2998ab7e1661d2
Rinat <lich666black@gmail.com>
2020-11-27 14:08:54 +0500

    replace account modal

--- src/components/modals/accounts.tsx
+++ src/components/modals/accounts.tsx
@@ -8,20 +8,17 @@
  */
 import React from 'react';
 import {
-  View,
-  Text,
   StyleSheet,
   ScrollView,
-  TouchableOpacity,
   Button,
   ViewStyle
 } from 'react-native';
 import { useStore } from 'effector-react';
-import { SvgXml } from 'react-native-svg';
 
 import Modal from 'react-native-modal';
 import { AccountItem } from 'app/components/account-item';
-import { DeleteIconSVG } from 'app/components/svg';
+import { ModalTitle } from 'app/components/modal-title';
+import { ModalWrapper } from 'app/components/modal-wrapper';
 
 import { keystore } from 'app/keystore';
 import i18n from 'app/lib/i18n';
@@ -66,15 +63,10 @@ export const AccountsModal: React.FC<Prop> = ({
       }}
       onBackdropPress={onTriggered}
     >
-      <View style={[styles.modalContainer, style]}>
-        <View style={styles.titleWrapper}>
-          <Text style={styles.title}>
-            {title}
-          </Text>
-          <TouchableOpacity onPress={onTriggered}>
-            <SvgXml xml={DeleteIconSVG}/>
-          </TouchableOpacity>
-        </View>
+      <ModalWrapper style={style}>
+        <ModalTitle onClose={onTriggered}>
+          {title}
+        </ModalTitle>
         <ScrollView style={{ marginVertical: 20 }}>
           {accounts.map((account, index) => (
             <AccountItem
@@ -93,29 +85,10 @@ export const AccountsModal: React.FC<Prop> = ({
             onPress={onAdd}
           />
         ) : null}
-      </View>
+      </ModalWrapper>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
-  title: {
-    color: theme.colors.white,
-    fontSize: 17,
-    lineHeight: 22
-  },
-  modalContainer: {
-    paddingHorizontal: 15,
-    borderTopEndRadius: 16,
-    borderTopStartRadius: 16,
-    backgroundColor: '#18191D',
-    justifyContent: 'space-between',
-    paddingVertical: 15
-  },
-  titleWrapper: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    paddingTop: 15
-  }
 });
