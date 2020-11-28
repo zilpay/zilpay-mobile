f931b567bbf56d6d638ca080e2373e8217c81346
Rinat <lich666black@gmail.com>
2020-11-28 13:21:38 +0500

    updated config

--- src/config/explorer.ts
+++ src/config/explorer.ts
@@ -8,7 +8,7 @@
  */
 
 export const VIEW_BLOCK_URL = 'https://viewblock.io/zilliqa';
-export const VIEW_BLOCK_API = 'https://api.viewblock.io/v1/zilliqa';
+export const VIEW_BLOCK_API_V1 = 'https://api.viewblock.io/v1/zilliqa';
 export const API_KEY = '2645e676e86b7a87761ddf3336129c2abc6205b17cf37580c382739e5fb14461';
 export const TX_DIRECTION = {
   in: 'in',
