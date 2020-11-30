d0655a130a0063c3c6f4ac42c61f81e876088c95
Rinat <lich666black@gmail.com>
2020-11-30 19:00:51 +0500

    optimized meemory

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ import './shim';
 import { AppRegistry } from 'react-native';
 import root from './src/root';
 import { name as appName } from './app.json';
+import { enableScreens } from 'react-native-screens';
+
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => root);
