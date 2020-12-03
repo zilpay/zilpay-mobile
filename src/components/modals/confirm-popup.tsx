3870f2ad118303d0fb8416b1346827ed60bbf651
Rinat <lich666black@gmail.com>
2020-12-03 17:55:49 +0500

    added confirm-popup

--- src/components/modals/confirm-popup.tsx
+++ src/components/modals/confirm-popup.tsx
@@ -10,26 +10,66 @@ import React from 'react';
 import {
   StyleSheet,
   ScrollView,
-  ViewStyle
+  Text,
+  ViewStyle,
+  View
 } from 'react-native';
+import { SvgXml, SvgCssUri } from 'react-native-svg';
 
+import {
+  ProfileSVG,
+  ReceiveIconSVG,
+  AmountIconSVG
+} from 'app/components/svg';
+import commonStyles from 'app/components/transfer/styles';
+import { AdvacedGas } from 'app/components/advaced-gas';
 import Modal from 'react-native-modal';
+import { CustomButton } from 'app/components/custom-button';
 import { ModalTitle } from 'app/components/modal-title';
 import { ModalWrapper } from 'app/components/modal-wrapper';
+
+import { Token, Account } from 'types';
+import { TOKEN_ICONS } from 'app/config';
+import i18n from 'app/lib/i18n';
+import { fromZil, toConversion, trim } from 'app/filters';
+import { keystore } from 'app/keystore';
 
 type Prop = {
   style?: ViewStyle;
   visible: boolean;
+  token: Token;
+  account: Account;
   title: string;
+  amount: string;
+  netwrok: string;
+  recipient: string;
   onTriggered: () => void;
 };
 
 export const ConfirmPopup: React.FC<Prop> = ({
   title,
   style,
+  token,
+  account,
+  netwrok,
+  amount,
   visible,
+  recipient,
   onTriggered
 }) => {
+  const settingsState = keystore.settings.store.useValue();
+  const currencyState = keystore.currency.store.useValue();
+  const gasState = keystore.gas.store.useValue();
+
+  const [DS, setDS] = React.useState(false);
+  const [gas, setGas] = React.useState(gasState);
+
+  const conversion = React.useMemo(() => {
+    const rate = settingsState.rate[currencyState];
+    const balance = account.balance[netwrok][token.symbol];
+
+    return toConversion(balance, rate, token.decimals);
+  }, [token, account, netwrok, settingsState, currencyState]);
 
   return (
     <Modal
@@ -46,9 +86,70 @@ export const ConfirmPopup: React.FC<Prop> = ({
           style={{ padding: 15 }}
           onClose={onTriggered}
         >
+          <SvgCssUri
+            height="30"
+            width="30"
+            uri={`${TOKEN_ICONS}/${token.symbol}.svg`}
+          />
           {title}
         </ModalTitle>
-        <ScrollView style={{ marginVertical: 20 }}>
+        <ScrollView style={{ marginBottom: 15 }}>
+          <View style={commonStyles.item}>
+            <SvgXml xml={ProfileSVG} />
+            <View style={[commonStyles.itemInfo, styles.item]}>
+              <Text style={commonStyles.label}>
+                {i18n.t('transfer_account')}
+              </Text>
+              <View style={commonStyles.infoWrapper}>
+                <Text style={commonStyles.nameAmountText}>
+                  {account.name}
+                </Text>
+                <Text style={commonStyles.addressAmount}>
+                  {trim(account[settingsState.addressFormat])}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={commonStyles.item}>
+            <SvgXml xml={AmountIconSVG} />
+            <View style={[commonStyles.itemInfo, styles.item]}>
+              <Text style={commonStyles.label}>
+                {i18n.t('transfer_amount')}
+              </Text>
+              <View style={commonStyles.infoWrapper}>
+                <Text style={commonStyles.nameAmountText}>
+                  {fromZil(amount, token.decimals)}
+                </Text>
+                <Text style={commonStyles.addressAmount}>
+                  {conversion} {currencyState}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <View style={commonStyles.item}>
+            <SvgXml xml={ReceiveIconSVG} />
+            <View style={[commonStyles.itemInfo, styles.item]}>
+              <Text style={commonStyles.label}>
+                {i18n.t('recipient_account')}
+              </Text>
+              <View style={commonStyles.infoWrapper}>
+                <Text style={commonStyles.nameAmountText}>
+                  {trim(recipient)}
+                </Text>
+              </View>
+            </View>
+          </View>
+          <AdvacedGas
+            gas={gas}
+            ds={DS}
+            onDSChanged={setDS}
+            onChange={setGas}
+          />
+          <CustomButton
+            title={i18n.t('send')}
+            style={styles.sendBtn}
+            onPress={() => null}
+          />
         </ScrollView>
       </ModalWrapper>
     </Modal>
@@ -56,4 +157,10 @@ export const ConfirmPopup: React.FC<Prop> = ({
 };
 
 const styles = StyleSheet.create({
+  item: {
+    paddingBottom: 10
+  },
+  sendBtn: {
+    marginVertical: 15
+  }
 });
