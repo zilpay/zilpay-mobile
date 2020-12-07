ff18375a820919777a9c92d0daa8909d63c6ba9f
Rinat <lich666black@gmail.com>
2020-12-07 18:40:01 +0500

    aded errors hanlder and right amount calc

--- src/components/transfer/amount.tsx
+++ src/components/transfer/amount.tsx
@@ -18,25 +18,31 @@ import {
   Dimensions
 } from 'react-native';
 import { SvgXml } from 'react-native-svg';
+import Big from 'big.js';
 
 import {
   AmountIconSVG
 } from 'app/components/svg';
 
 import i18n from 'app/lib/i18n';
-import { fromZil, amountFromPercent } from 'app/filters';
-import { Token, Account } from 'types';
+import { fromZil, toQA } from 'app/filters';
+import { Amount } from 'app/utils';
+import { Token, Account, GasState } from 'types';
 
 import styles from './styles';
 import { theme } from 'app/styles';
+
+Big.PE = 99;
 
 type Prop = {
   style?: ViewStyle;
   token: Token;
   account: Account;
+  gas: GasState;
   netwrok: string;
   value: string;
   onChange: (amount: string) => void;
+  onError: (error: boolean) => void;
 };
 
 const percents = [0, 25, 50, 75, 100];
@@ -44,28 +50,45 @@ const { width } = Dimensions.get('window');
 export const TransferAmount: React.FC<Prop> = ({
   style,
   token,
+  gas,
   netwrok,
   account,
   value,
-  onChange
+  onChange,
+  onError
 }) => {
+  const [error, setError] = React.useState(false);
+
   const balance = React.useMemo(
     () => account.balance[netwrok][token.symbol],
     [token, netwrok, account]
   );
 
   const handleChnage = React.useCallback((amount) => {
-    onChange(Number(amount).toString());
-  }, [onChange, token]);
+    setError(false);
+
+    const _amount = toQA(amount, token.decimals);
+    const isInsufficientFunds = new Amount(gas, balance)
+      .insufficientFunds(_amount, token);
+
+    setError(isInsufficientFunds);
+    onError(isInsufficientFunds);
+
+    onChange(Big(amount).toString());
+  }, [onChange, token, gas, balance, onError]);
   /**
    * Useing percents for calculate amount.
    */
   const handlePercentSelect = React.useCallback((percent) => {
-    const newAmount = amountFromPercent(balance, percent);
-    const zils = fromZil(newAmount, token.decimals, false);
+    setError(false);
+
+    const amount = new Amount(gas, balance)
+      .fromPercent(percent, token).toString();
+    const zils = fromZil(amount, token.decimals, false);
 
     onChange(zils);
-  }, [balance, onChange, token]);
+    onError(error);
+  }, [balance, onChange, onError, token, gas]);
 
   return (
     <React.Fragment>
@@ -78,9 +101,13 @@ export const TransferAmount: React.FC<Prop> = ({
           alignItems: 'center'
         }}>
           <SvgXml xml={AmountIconSVG} />
-          <View style={styles.inputWrapper}>
+          <View style={[styles.inputWrapper, {
+            borderBottomColor: error ? theme.colors.danger : '#8A8A8F'
+          }]}>
             <TextInput
-              style={commonStyles.input}
+              style={[commonStyles.input, {
+                color: error ? theme.colors.danger : theme.colors.white
+              }]}
               keyboardType={'numeric'}
               placeholder={i18n.t('transfer_amount')}
               placeholderTextColor="#8A8A8F"
@@ -113,8 +140,8 @@ export const TransferAmount: React.FC<Prop> = ({
 
 const commonStyles = StyleSheet.create({
   input: {
-    color: theme.colors.white,
     width: width - 100,
+    fontSize: 17,
     padding: 10
   }
 });
