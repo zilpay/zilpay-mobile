c7ae432508667ae4b83ef648762af9df1e71a62b
Rinat <lich666black@gmail.com>
2020-12-08 21:57:18 +0500

    added error hanlder and full done privKey import account

--- src/components/create-account/import.tsx
+++ src/components/create-account/import.tsx
@@ -10,36 +10,71 @@
 import React from 'react';
 import {
   View,
-  Button,
+  Text,
   TextInput,
   Dimensions,
-  Text,
   StyleSheet
 } from 'react-native';
-import { SvgXml } from 'react-native-svg';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-import { ProfileSVG, LockSVG } from 'app/components/svg';
 import { CustomButton } from 'app/components/custom-button';
 import { Selector } from 'app/components/selector';
+import { AccountName } from 'app/components/account-name';
 
 import i18n from 'app/lib/i18n';
 import { keystore } from 'app/keystore';
 import { theme } from 'app/styles';
+
+type Prop = {
+  onImported: () => void;
+};
 
 const { height } = Dimensions.get('window');
 const variants = [
   'Private Key',
   'Ledger'
 ];
-export const ImportAccount: React.FC = ({
+export const ImportAccount: React.FC<Prop> = ({
+  onImported
 }) => {
+  const [loading, setLoading] = React.useState(false);
   const [selected, setSelected] = React.useState(variants[0]);
-  const [ledgerIndex, setLedgerIndex] = React.useState(0);
+  const [accName, setAccName] = React.useState(`Imported ${keystore.account.lastIndexLedger}`);
+  const [ledgerIndex, setLedgerIndex] = React.useState(
+    keystore.account.lastIndexLedger
+  );
+  const [privateKey, setPrivateKey] = React.useState<string | null>(null);
+  const [privKeyErr, setPrivKeyErr] = React.useState('');
 
-  const hanldeChange = React.useCallback(() => {
-    //
-  }, []);
+  const disabled = React.useMemo(() => {
+    if (variants[0] === selected) {
+      return !privateKey || !accName;
+    } else if (variants[1] === selected) {
+      return isNaN(Number(ledgerIndex)) || !accName;
+    }
+  }, [privateKey, accName, ledgerIndex]);
+
+  const handleimport = React.useCallback(async() => {
+    setLoading(true);
+    if (variants[0] === selected && privateKey) {
+      try {
+        const account = await keystore
+          .account
+          .fromPrivateKey(privateKey, accName);
+        await keystore.account.add(account);
+
+        onImported();
+      } catch (err) {
+        setPrivKeyErr(err.message);
+      }
+    }
+
+    if (variants[1] === selected) {
+      //
+    }
+
+    setLoading(false);
+  }, [privateKey, selected, accName, onImported, setPrivKeyErr]);
   const hanldeLedgerChange = React.useCallback((num) => {
     const index = Number(num);
 
@@ -47,6 +82,24 @@ export const ImportAccount: React.FC = ({
       setLedgerIndex(index);
     }
   }, [setLedgerIndex]);
+  const handleChangeType = React.useCallback((value) => {
+    setSelected(value);
+    setPrivKeyErr('');
+
+    if (value === variants[0]) {
+      setAccName(i18n.t('acc_name_key', {
+        index: keystore.account.lastIndexPrivKey
+      }));
+    } else if (value === variants[1]) {
+      setAccName(i18n.t('acc_name_ledger', {
+        index: keystore.account.lastIndexLedger
+      }));
+    }
+  }, [setSelected, setAccName]);
+  const hanldeChangePrivKey = React.useCallback((value) => {
+    setPrivKeyErr('');
+    setPrivateKey(value);
+  }, [setPrivateKey, setPrivKeyErr]);
 
   return (
     <KeyboardAwareScrollView>
@@ -54,29 +107,43 @@ export const ImportAccount: React.FC = ({
         style={{ backgroundColor: 'transparent' }}
         items={variants}
         selected={selected}
-        onSelect={setSelected}
+        onSelect={handleChangeType}
       />
       <View style={styles.wrapper}>
+        <AccountName
+          style={{ paddingVertical: 15}}
+          name={accName}
+          setName={setAccName}
+        />
         {variants[0] === selected ? (
-          <TextInput
-            multiline={true}
-            numberOfLines={10}
-            style={styles.text}
-            placeholder={i18n.t('import_private_key_placeholder')}
-            placeholderTextColor="#8A8A8F"
-            onChangeText={hanldeChange}
-          />
+          <View>
+            <TextInput
+              multiline={true}
+              numberOfLines={10}
+              style={styles.text}
+              placeholder={i18n.t('import_private_key_placeholder')}
+              placeholderTextColor="#8A8A8F"
+              onChangeText={hanldeChangePrivKey}
+            />
+            <Text style={styles.errorMessage}>
+              {privKeyErr}
+            </Text>
+          </View>
         ) : null}
         {variants[1] === selected ? (
           <TextInput
-            defaultValue={String(ledgerIndex)}
+            value={String(ledgerIndex)}
             keyboardType={'numeric'}
             style={styles.text}
             onChangeText={hanldeLedgerChange}
           />
         ) : null}
         <CustomButton
+          style={{ marginTop: 15 }}
           title={i18n.t('import_account')}
+          disabled={disabled}
+          isLoading={loading}
+          onPress={handleimport}
         />
       </View>
     </KeyboardAwareScrollView>
@@ -88,15 +155,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     justifyContent: 'space-around'
   },
+  errorMessage: {
+    color: theme.colors.danger,
+    fontSize: 13,
+    marginLeft: 5,
+    lineHeight: 22
+  },
   text: {
-    marginTop: 60,
+    marginTop: 15,
     height: height / 10,
     borderColor: '#8A8A8F',
     borderWidth: 1,
     borderRadius: 8,
     color: theme.colors.white,
     padding: 20,
-    fontSize: 23,
-    marginBottom: 30
+    fontSize: 23
   }
 });
