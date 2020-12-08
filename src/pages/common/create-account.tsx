254e74eedfb4a820af5b33e3a81ce57b6b5f2cd7
Rinat <lich666black@gmail.com>
2020-12-08 20:26:50 +0500

    remake create-account-page

--- src/pages/common/create-account.tsx
+++ src/pages/common/create-account.tsx
@@ -13,108 +13,84 @@ import {
   Text,
   TextInput,
   Button,
+  Dimensions,
   SafeAreaView,
   StyleSheet
 } from 'react-native';
-import { SvgXml } from 'react-native-svg';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { TabView, SceneMap } from 'react-native-tab-view';
 
-import { ProfileSVG, LockSVG } from 'app/components/svg';
+import {
+  AddAccount,
+  CreateAccountNavBar,
+  ImportAccount
+} from 'app/components/create-account';
 
 import { theme } from 'app/styles';
 import i18n from 'app/lib/i18n';
 import { keystore } from 'app/keystore';
-// import { MAX_NAME_DIFFICULTY } from 'app/config';
 import { RootParamList } from 'app/navigator';
 
 type Prop = {
   navigation: StackNavigationProp<RootParamList>;
 };
 
+const { width } = Dimensions.get('window');
+const initialLayout = { width };
+
+enum Tabs {
+  add = 'add',
+  import = 'import'
+}
+
 export const CreateAccountPage: React.FC<Prop> = ({ navigation }) => {
   const accountState = keystore.account.store.useValue();
   const authState = keystore.guard.auth.store.useValue();
 
-  const [password, setPassword] = React.useState('');
-  const [passwordError, setPasswordError] = React.useState(' ');
-  const [name, setName] = React.useState(i18n.t('create_account_name', {
-    number: accountState.identities.length
-  }));
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: Tabs.add, title: i18n.t('add_account') },
+    { key: Tabs.import, title: i18n.t('import_account') }
+  ]);
 
-  const handleCreate = React.useCallback(async() => {
-    if (authState.biometricEnable) {
-      await keystore.addNextAccount(name);
-      await keystore.transaction.sync();
+  const handleCreate = React.useCallback(() => {
+    navigation.navigate('App', {
+      screen: 'Home'
+    });
+  }, []);
 
-      return navigation.navigate('App', {
-        screen: 'Home'
-      });
-    }
-
-    try {
-      await keystore.addNextAccount(name, password);
-      await keystore.transaction.sync();
-
-      return navigation.navigate('App', {
-        screen: 'Home'
-      });
-    } catch (err) {
-      setPasswordError(i18n.t('lock_error'));
-    }
-  }, [password, name]);
+  const renderScene = SceneMap({
+    [Tabs.add]: () => (
+      <AddAccount
+        biometricEnable={authState.biometricEnable}
+        newIndex={accountState.identities.length}
+        onAdded={handleCreate}
+      />
+    ),
+    [Tabs.import]: ImportAccount
+  });
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>
         {i18n.t('create_account_title')}
       </Text>
-      <KeyboardAwareScrollView style={styles.wrapper}>
-        <View style={styles.elementWrapper}>
-          <View style={styles.inputWrapper}>
-            <SvgXml xml={ProfileSVG} />
-            <TextInput
-              style={styles.textInput}
-              placeholder={i18n.t('pass_setup_input0')}
-              defaultValue={name}
-              placeholderTextColor="#2B2E33"
-              onChangeText={setName}
-            />
-          </View>
-          <Text style={styles.label}>
-            {i18n.t('pass_setup_label0')}
-          </Text>
-        </View>
-        {!authState.biometricEnable ? (
-          <View style={styles.elementWrapper}>
-            <View style={styles.inputWrapper}>
-              <SvgXml xml={LockSVG} />
-              <TextInput
-                style={styles.textInput}
-                secureTextEntry={true}
-                placeholder={i18n.t('pass_setup_input1')}
-                placeholderTextColor="#2B2E33"
-                onChangeText={setPassword}
-              />
-              <Text style={styles.errorMessage}>
-                {passwordError}
-              </Text>
-            </View>
-          </View>
-        ) : null}
-      </KeyboardAwareScrollView>
-      <View style={{ marginBottom: '10%' }}>
-        <Button
-          title={i18n.t('create_account_btn')}
-          color={theme.colors.primary}
-          onPress={handleCreate}
-        />
-      </View>
+      <TabView
+        style={styles.tabView}
+        renderTabBar={CreateAccountNavBar}
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={initialLayout}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: theme.colors.black
@@ -127,36 +103,7 @@ const styles = StyleSheet.create({
     lineHeight: 41,
     marginTop: 30
   },
-  errorMessage: {
-    color: theme.colors.danger,
-    marginTop: 4,
-    lineHeight: 22
-  },
-  wrapper: {
-    paddingHorizontal: 20,
-    marginTop: 40,
-    marginBottom: 60
-  },
-  elementWrapper: {
-    marginVertical: 35
-  },
-  label: {
-    marginVertical: 5,
-    color: '#8A8A8F',
-    marginLeft: 38
-  },
-  textInput: {
-    fontSize: 17,
-    lineHeight: 22,
-    padding: 10,
-    borderBottomColor: '#2B2E33',
-    borderBottomWidth: 1,
-    color: theme.colors.white,
-    width: '90%'
-  },
-  inputWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between'
-  },
+  tabView: {
+    backgroundColor: theme.colors.black
+  }
 });
