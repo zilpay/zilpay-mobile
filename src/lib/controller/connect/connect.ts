32f27ce110e8840e1ff9a7986c63563bb5f27c41
Rinat <lich666black@gmail.com>
2020-12-18 19:26:11 +0500

    added connect Controller

---
+++ src/lib/controller/connect/connect.ts
@@ -0,0 +1,55 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+import {
+  connectStore,
+  connectStoreAdd,
+  connectStoreReset,
+  connectStoreUpdate
+} from './store';
+import { MobileStorage, buildObject } from "app/lib/storage";
+import { Connect } from 'types';
+import { STORAGE_FIELDS } from 'app/config';
+
+export class ConnectController {
+  public store = connectStore;
+  private _storage: MobileStorage;
+
+  constructor(storage: MobileStorage) {
+    this._storage = storage;
+  }
+
+  public async add(connect: Connect) {
+    connectStoreAdd(connect);
+
+    await this.sync();
+  }
+
+  public async reset() {
+    connectStoreReset();
+
+    await this._storage.set(
+      buildObject(STORAGE_FIELDS.CONNECTIONS, this.store.get())
+    );
+  }
+
+  public async sync() {
+    const data = await this._storage.get(STORAGE_FIELDS.CONNECTIONS);
+
+    if (typeof data !== 'string') {
+      await this.reset();
+      return null;
+    }
+
+    try {
+      connectStoreUpdate(JSON.parse(data));
+    } catch {
+      await this.reset();
+    }
+  }
+}
---
+++ src/lib/controller/connect/store.ts
@@ -0,0 +1,25 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+import { Connect } from 'types';
+import { newRidgeState } from 'react-ridge-state';
+
+const initalState: Connect[] = [];
+export const connectStore = newRidgeState<Connect[]>(initalState);
+
+export function connectStoreUpdate(payload: Connect[]) {
+  connectStore.set(() => payload);
+}
+
+export function connectStoreAdd(payload: Connect) {
+  connectStore.set((state) => ([...state, payload]));
+}
+
+export function connectStoreReset() {
+  connectStore.reset();
+}
