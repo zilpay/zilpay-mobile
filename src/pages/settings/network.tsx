93b9d8a926ba362c93347ac4f8f12f119dab23dd
Rinat <lich666black@gmail.com>
2021-03-06 11:00:09 +0500

    updated deps

--- src/pages/settings/network.tsx
+++ src/pages/settings/network.tsx
@@ -62,6 +62,7 @@ export const NetworkPage = () => {
       await keystore.network.changeNetwork(net);
 
       if (net !== custom) {
+        await keystore.network.sync();
         await keystore.transaction.sync();
         await keystore.ssn.sync();
         await keystore.settings.sync();
