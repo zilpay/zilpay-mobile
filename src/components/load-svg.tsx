29e7f38c8ed8a87951381979b95f8d862411fe1b
Rinat <lich666black@gmail.com>
2021-05-13 19:13:34 +0500

    fixed viewblock API when retunr png

--- src/components/load-svg.tsx
+++ src/components/load-svg.tsx
@@ -13,6 +13,7 @@ import {
 } from 'react-native';
 import { useTheme } from '@react-navigation/native';
 import { SvgCss } from 'react-native-svg';
+import FastImage from 'react-native-fast-image';
 
 import HelpIconSVG from 'app/assets/icons/help.svg';
 
@@ -28,17 +29,24 @@ type Prop = {
 export const LoadSVG: React.FC<Prop> = ({ addr, height, width }) => {
   const { dark, colors } = useTheme();
   const [xml, setXml] = React.useState<string | null>(null);
+  const [url, setUrl] = React.useState<string | null>(null);
 
   React.useEffect(() => {
     const uri = viewIcon(addr, dark);
 
     fetch(uri)
       .then((res) => {
-        if (res.ok) {
+        const type = String(res.headers['map']['content-type']);
+
+        if (!res.ok) {
+          return null;
+        } else if (type.includes('svg')) {
           return res.text();
+        } else if (type.includes('png')) {
+          setUrl(uri);
         }
 
-        return undefined;
+        return null;
       })
       .then((content) => {
         setXml(content ? content : HelpIconSVG());
@@ -48,21 +56,33 @@ export const LoadSVG: React.FC<Prop> = ({ addr, height, width }) => {
       });
   }, [addr, dark]);
 
+  if (url) {
+    return (
+      <FastImage
+        source={{ uri: url }}
+        style={{
+          width: Number(width),
+          height: Number(height)
+        }}
+      />
+    );
+  }
+
+  if (xml) {
+    return (
+      <SvgCss
+        width={width}
+        height={height}
+        xml={xml}
+      />
+    );
+  }
+
   return (
-    <React.Fragment>
-      {xml ? (
-        <SvgCss
-          width={width}
-          height={height}
-          xml={xml}
-        />
-      ) : (
-        <ActivityIndicator
-          animating={!xml}
-          color={colors.primary}
-        />
-      )}
-    </React.Fragment>
+    <ActivityIndicator
+      animating={true}
+      color={colors.primary}
+    />
   );
 };
 
