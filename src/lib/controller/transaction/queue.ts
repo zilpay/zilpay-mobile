1fedcf08b6daf549bcf8b58980d2db3d75da768c
Rinat <lich666black@gmail.com>
2021-10-26 15:47:13 +0300

    fixed net txns check

--- src/lib/controller/transaction/queue.ts
+++ src/lib/controller/transaction/queue.ts
@@ -148,6 +148,8 @@ export class TransactionsQueue {
   }
 
   public async checkProcessedTx() {
+    const netwrok = new NetworkControll(this._storage, true);
+    const zilliqa = new ZilliqaControl(netwrok);
     const list = this.store.get();
     const now = new Date().getTime();
     const dilaySeconds = 15000;
@@ -159,9 +161,9 @@ export class TransactionsQueue {
       return null;
     }
     const requests = identities.map(({ hash }) => {
-      return this._zilliqa.provider.buildBody(Methods.GetTransactionStatus, [hash]);
+      return zilliqa.provider.buildBody(Methods.GetTransactionStatus, [hash]);
     });
-    let replies = await this._zilliqa.sendJson(...requests);
+    let replies = await zilliqa.sendJson(...requests);
     if (!Array.isArray(replies)) {
       replies = [replies];
     }
