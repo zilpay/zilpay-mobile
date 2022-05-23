567036bfc9b8fe935986cc8939b56426b8e10898
Rinat <lich666black@gmail.com>
2022-05-23 19:16:47 +0300

    added smart-input for swap

---
+++ src/components/swap/swap-input.tsx
@@ -0,0 +1,180 @@
+/*
+ * Project: ZilPay-wallet
+ * Author: Rinat(lich666dead)
+ * -----
+ * Modified By: the developer formerly known as Rinat(lich666dead) at <lich666black@gmail.com>
+ * -----
+ * Copyright (c) 2020 ZilPay
+ */
+
+import type { Token } from 'types';
+
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+  ViewStyle,
+  Dimensions,
+  StyleProp
+} from 'react-native';
+import { useTheme } from '@react-navigation/native';
+import Big from 'big.js';
+
+import { LoadSVG } from 'app/components/load-svg';
+import ArrowIconSVG from 'app/assets/icons/arrow.svg';
+import { Button } from 'app/components/button';
+
+import i18n from 'app/lib/i18n';
+
+import { fonts } from 'app/styles';
+import { toBech32Address } from 'app/utils/bech32';
+import { nFormatter, toConversion } from 'app/filters';
+
+Big.PE = 99;
+
+type Prop = {
+  token: Token;
+  net: string;
+  value: string;
+  title: string;
+  currency: string;
+  rate: number;
+  disabled?: boolean;
+  balance?: string;
+  containerStyles?: StyleProp<ViewStyle>;
+  pecrents?: number[];
+  onChose?: () => void;
+  onChange?: (value: string) => void;
+};
+
+const { width } = Dimensions.get('window');
+export const SwapInput: React.FC<Prop> = ({
+  token,
+  net,
+  value,
+  title,
+  rate,
+  containerStyles,
+  currency,
+  balance = '0',
+  disabled = false,
+  pecrents = [10, 25, 50, 70, 100],
+  onChose = () => null,
+  onChange = () => null
+}) => {
+  const { colors } = useTheme();
+
+  const conversion = React.useMemo(() => {
+    const tokenRate = rate * (token.rate || 0);
+    const amount = Big(value).mul(10 ** token.decimals);
+
+    return toConversion(String(amount), tokenRate, token.decimals);
+  }, [token, rate, value]);
+
+  const hanldeOnPercent = React.useCallback((percent: number) => {
+    try {
+      const amount = Big(balance).div(10 ** token.decimals);
+      const hundred = Big('100');
+      const nPercent = Big(percent);
+      const nAmount = Big(amount);
+      onChange(String(nAmount.mul(nPercent).div(hundred)));
+    } catch {
+      ///
+    }
+  }, [balance, token]);
+
+  return (
+    <View style={[styles.container, {
+      backgroundColor: colors.card
+    }, containerStyles]}>
+      <Text style={[styles.inputTitle, {
+        color: colors.notification
+      }]}>
+        {title}
+      </Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          keyboardType={'numeric'}
+          placeholder={i18n.t('transfer_amount')}
+          placeholderTextColor={colors.border}
+          value={value}
+          editable={!disabled}
+          onChangeText={onChange}
+        />
+        <TouchableOpacity
+          style={styles.iconWrapper}
+          onPress={onChose}
+        >
+          <LoadSVG
+            addr={toBech32Address(token.address[net])}
+            height="30"
+            width="30"
+          />
+          <Text style={[styles.symbol, {
+            color: colors.primary
+          }]}>
+            {token.symbol}
+          </Text>
+          <ArrowIconSVG fill={colors.primary} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.inputWrapper}>
+        <Text style={[styles.inputTitle, {
+          color: colors.notification
+        }]}>
+          {nFormatter(conversion)} {currency}
+        </Text>
+        <View style={styles.iconWrapper}>
+          {pecrents.map((p, key) => (
+            <Button
+              key={key}
+              title={`${p}%`}
+              color={colors.primary}
+              textStyle={styles.percent}
+              onPress={() => hanldeOnPercent(p)}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    fontFamily: fonts.Demi,
+    fontSize: 20,
+    paddingVertical: 6,
+    width: width - 140
+  },
+  container: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 10
+  },
+  inputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputTitle: {
+    fontFamily: fonts.Regular,
+    fontSize: 12
+  },
+  iconWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  symbol: {
+    fontFamily: fonts.Bold,
+    paddingHorizontal: 5
+  },
+  percent: {
+    fontSize: 14,
+    paddingHorizontal: 4
+  }
+});
--- src/pages/common/swap.tsx
+++ src/pages/common/swap.tsx
@@ -7,28 +7,103 @@
  * Copyright (c) 2021 ZilPay
  */
 
+import type { Token } from 'types';
+
 import React from 'react';
 import {
-  StyleSheet
+  StyleSheet,
+  View
 } from 'react-native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { useTheme } from '@react-navigation/native';
+import Big from 'big.js';
+import { RouteProp, useTheme } from '@react-navigation/native';
 
 import { SafeWrapper } from 'app/components/safe-wrapper';
 
-import { RootParamList } from 'app/navigator';
+import { CustomButton } from 'app/components/custom-button';
+import { SwapInput } from 'app/components/swap';
+
+import i18n from 'app/lib/i18n';
+import { CommonStackParamList } from 'app/navigator/common';
+import { keystore } from 'app/keystore';
+import { deppUnlink } from 'app/utils/deep-unlink';
+
 
 type Prop = {
-  navigation: StackNavigationProp<RootParamList>;
+  route: RouteProp<CommonStackParamList, 'SwapPage'>;
 };
 
-export const SwapPage: React.FC<Prop> = ({ navigation }) => {
+interface Pair {
+  value: string;
+  meta: Token
+}
+
+export const SwapPage: React.FC<Prop> = ({ route }) => {
   const { colors } = useTheme();
+
+  const tokensState = keystore.token.store.useValue();
+  const networkState = keystore.network.store.useValue();
+  const accountState = keystore.account.store.useValue();
+  const settingsState = keystore.settings.store.useValue();
+  const currencyState = keystore.currency.store.useValue();
+
+  const [pair, setPair] = React.useState<Pair[]>([
+    {
+      value: '50',
+      meta: tokensState[0]
+    },
+    {
+      value: '0',
+      meta: tokensState[1]
+    }
+  ]);
+
+  const account = React.useMemo(
+    () => accountState.identities[accountState.selectedAddress],
+    [accountState]
+  );
+
+
+  const hanldeInput = React.useCallback((value: string) => {
+    try {
+      const newPair = deppUnlink<Pair[]>(pair);
+      newPair[0].value = value;
+
+      setPair(newPair);
+    } catch {
+      ////
+    }
+  }, [pair]);
+
 
   return (
     <SafeWrapper style={[styles.container, {
       backgroundColor: colors.background
     }]}>
+      <View>
+        <SwapInput
+          token={pair[0].meta}
+          currency={currencyState}
+          rate={settingsState.rate[currencyState]}
+          net={networkState.selected}
+          value={pair[0].value}
+          title={'You pay'}
+          balance={account.balance[networkState.selected][pair[0].meta.symbol]}
+          containerStyles={styles.input}
+          onChange={hanldeInput}
+        />
+        <SwapInput
+          token={pair[1].meta}
+          currency={currencyState}
+          rate={settingsState.rate[currencyState]}
+          net={networkState.selected}
+          value={pair[1].value}
+          title={'you get'}
+          pecrents={[]}
+          disabled
+          containerStyles={styles.input}
+        />
+      </View>
+      <CustomButton title={i18n.t('swap')}/>
     </SafeWrapper>
   );
 };
@@ -36,8 +111,9 @@ export const SwapPage: React.FC<Prop> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    borderTopEndRadius: 16,
-    borderTopStartRadius: 16
+    padding: 10
+  },
+  input: {
+    marginVertical: 5
   }
 });
