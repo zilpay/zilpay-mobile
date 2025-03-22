3bb2962aa3936adeb8197e13b947ed55599ba442
Rinat <rinat@Rinats-MacBook-Air.local>
2025-03-22 22:52:13 +0900

    fixed fformat

--- lib/config/ftokens.dart
+++ lib/config/ftokens.dart
@@ -1,4 +1,4 @@
 const zeroEVM = "0x0000000000000000000000000000000000000000";
 const zeroZIL = "zil1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq9yf6pz";
 
-const baseThreshold = 0.00000000000001;
+const baseThreshold = 0.0000000001;
