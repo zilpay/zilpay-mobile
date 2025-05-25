58370041ec3a119e62d990485080790b0a7c76bb
Rinat <rinat@Rinats-MacBook-Air.local>
2025-05-25 11:59:20 +0900

    updated android sdk;

--- android/app/src/main/kotlin/com/example/zilpay/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/zilpay/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.zilpaymobile
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
+class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
