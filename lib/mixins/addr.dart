1126a0cae9e7a8311881c922ff1c1ac9884615f2
Rinat <you@example.com>
2026-01-04 18:17:14 +1000

    fix: improve address display format to show first, center, and last 4 characters

--- lib/mixins/addr.dart
+++ lib/mixins/addr.dart
@@ -1,10 +1,13 @@
-String shortenAddress(String address, {int leftSize = 6, int rightSize = 3}) {
-  if (address.length < (leftSize + rightSize)) {
+String shortenAddress(String address,
+    {int leftSize = 4, int rightSize = 4, int centerSize = 4}) {
+  if (address.length < (leftSize + rightSize + centerSize)) {
     return address;
   }
 
   final left = address.substring(0, leftSize);
   final right = address.substring(address.length - rightSize);
+  final centerStart = (address.length - centerSize) ~/ 2;
+  final center = address.substring(centerStart, centerStart + centerSize);
 
-  return '$left..$right';
+  return '$left...$center...$right';
 }
