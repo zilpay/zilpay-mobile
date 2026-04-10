d182827e189ac2c8fae697a43b2ddce937367386
hicaru <hicaru@hicarus-MacBook-Air.local>
2026-04-10 22:40:18 +0900

    fix: clear navigation stack on login to prevent back button returning to login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -66,7 +66,7 @@ class _LoginPageState extends State<LoginPage> with StatusBarMixin {
 
   void _navigateToHome() {
     _appState.setSelectedWallet(_selectedWallet);
-    Navigator.of(context).pushNamed('/');
+    Navigator.of(context).pushNamedAndRemoveUntil('/', (route) => false);
   }
 
   void _navigateToNewWallet() {
